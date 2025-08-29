3ea632f7047120b9f6dd5bb0d6b6155aa98e400a
therealpyppy <furghadmanar@gmail.com>
2025-08-28 19:51:05 -0500

    move nav to top left

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button"
 
 export default function Navigation() {
     return (
-        <nav className="fixed left-1/2 m-2 top-0 -translate-x-1/2 z-50">
+        <nav className="fixed m-2 top-0 z-50">
             <Button asChild size="sm">
                 <Link to="/">
                     Home
