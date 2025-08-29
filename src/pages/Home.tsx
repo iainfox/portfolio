9e6955f2c80c23751b833362550aeb4a6cdaf538
therealpyppy <furghadmanar@gmail.com>
2025-08-28 19:50:21 -0500

    align with brightness mode switcher

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { ModeToggle } from '@/components/ModeToggle'
 export default function Home() {
     return (
         <div>
-            <div className='ml-auto w-fit m-1'>
+            <div className='ml-auto w-fit m-2'>
                 <ModeToggle />
             </div>
         </div>
