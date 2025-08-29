c3a5c868a266e24b7b1147451cf7029ec6588110
therealpyppy <furghadmanar@gmail.com>
2025-08-28 19:47:58 -0500

    move brightness toggle to top right

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,8 +2,8 @@ import { ModeToggle } from '@/components/ModeToggle'
 
 export default function Home() {
     return (
-        <div className="min-h-screen grid place-items-center">
-            <div className="flex flex-col items-center gap-8">
+        <div>
+            <div className='ml-auto w-fit m-1'>
                 <ModeToggle />
             </div>
         </div>
