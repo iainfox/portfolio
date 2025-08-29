1d02dc36c8967e5551daa0d5b092cb20373c65ef
therealpyppy <furghadmanar@gmail.com>
2025-08-28 20:00:06 -0500

    move mode toggle to nav area

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,14 +1,19 @@
 import { Link } from 'react-router-dom'
 import { Button } from "@/components/ui/button"
+import { ModeToggle } from './ModeToggle'
 
 export default function Navigation() {
     return (
-        <nav className="fixed m-2 top-0 z-50">
-            <Button asChild size="sm">
+        <nav className="fixed m-2 top-0 z-50 w-full flex items-center">
+            <Button asChild>
                 <Link to="/">
                     Home
                 </Link>
             </Button>
+
+            <div className="w-fit ml-auto -translate-x-1/2">
+                <ModeToggle />
+            </div>
         </nav>
     )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,6 @@
-import { ModeToggle } from '@/components/ModeToggle'
-
 export default function Home() {
     return (
         <div>
-            <div className='ml-auto w-fit m-2'>
-                <ModeToggle />
-            </div>
         </div>
     )
 }
